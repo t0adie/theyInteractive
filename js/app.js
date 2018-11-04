4cb9f88408efe7033c775e0d5670159bdaa1660c
t0adie <derek@4dconcepts.ca>
2018-11-03 21:35:35 -0400

    Responsive
    Added more responsive layout functioality.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,22 @@
 $(document).foundation();
 
 var vw = $(window).width();
-var medOff = 'medium-offset';
+var offset = 'large-offset';
 var imgBox = $('.author .float-right');
 var paralaxImg = $('.hero .book');
 
 $(document).ready(dynamicImage);
 $(window).on('resize', dynamicImage);
-$(window).on('scroll', paralax);
-
-function paralax() {
-    
-}
 
 function dynamicImage(vw) {
 
     if (vw >= 1260) {
-        $('.author div.no').removeClass(medOff + '-1');
-        $('.author div.no').addClass(medOff + '-2');
+        $('.author div.no').removeClass(offset + '-1');
+        $('.author div.no').addClass(offset + '-2');
     }
     else if (vw >= 980) {
-        $('.author div.no').removeClass(medOff + '-2');
-        $('.author div.no').addClass(medOff + '-1');
+        $('.author div.no').removeClass(offset + '-2');
+        $('.author div.no').addClass(offset + '-1');
     }
     else if (vw <= 640) {
         imgBox.css({
@@ -30,11 +25,7 @@ function dynamicImage(vw) {
     }
 
     $('.paralax').css({
-        height: paralaxImg.outerHeight() + 100
+        height: paralaxImg.innerHeight() + 100
     })
-
-    $('.float-right').css({
-        backgroundSize: imgBox.outerWidth()
-    });
 
 }
