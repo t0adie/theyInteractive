282dd5c233cbf4ff51f8b0f86f81e0abdeb8501f
t0adie <derek@4dconcepts.ca>
2018-11-06 09:55:43 -0500

    UX Functionality

--- js/app.js
+++ js/app.js
@@ -5,17 +5,31 @@ var offset = 'large-offset';
 var imgBox = $('.author .float-right');
 var paralaxImg = $('.hero .book');
 
-
 $(document).ready(function () {
 
     paralaxImg.fadeIn('slow');
     $('.preloader').delay(1500).fadeOut('slow');
-    
+
     dynamicImage();
+
+    $('.link').each(function () {
+        
+        // Get the width of the parent a link for each SVG element
+        var parentWidth = $(this).parent().width();
+
+        $(this).css({
+            width: parentWidth
+        });
+
+        // Modify the length of the SVG path so it will center
+
+        $(this).html('<path d="M1 2h' + parentWidth + '" />')
+
+    });
 
 });
 
-$(window).on('resize', dynamicImage); 
+$(window).on('resize', dynamicImage);
 
 // Creates a dynamic height for the paralax background image.
 
@@ -23,6 +37,6 @@ function dynamicImage(vw) {
 
     $('.paralax').css({
         height: paralaxImg.innerHeight() + 100
-    })
+    });
 
 }
