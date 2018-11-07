80b7699dcaf47495e54302f8abbfc0af528a081f
t0adie <derek@4dconcepts.ca>
2018-11-07 00:35:08 -0500

    Tweeking
    Finalizing changes to style and responsivness.

--- js/app.js
+++ js/app.js
@@ -5,15 +5,57 @@ var offset = 'large-offset';
 var imgBox = $('.author .float-right');
 var paralaxImg = $('.hero .book');
 
+// Creates a dynamic height for the paralax background image.
+
+function dynamicImage(vw) {
+
+    $('.paralax').css({
+        height: paralaxImg.innerHeight() + 100
+    });
+
+}
+
+// Fades in elements for some UX flair
+
+function uxFade() {
+
+    var uxItem = $(".slide img");
+
+    uxItem.each(function () {
+
+        var uxCur = $(this);
+        var uxDir = uxCur.data("slide");
+        var uxWidth = uxCur.innerWidth();
+
+        // Hide all of the elements on their respective sides
+        $(this).attr('style', uxDir + ': ' + uxWidth + 'px').css({
+            opacity: 0
+        });
+
+        var winTop = $(window).scrollTop();
+        var winHeight = $(window).height();
+        var uxChocolate = winTop + (winHeight / 3);
+        var uxTop = uxCur.offset();
+
+        if (uxTop.top < uxChocolate) {
+            $(this).attr('style', uxDir + ': 0').css({
+                opacity: 1
+            });
+        }
+
+    });
+
+}
+
 $(document).ready(function () {
+
+    dynamicImage();
 
     paralaxImg.fadeIn('slow');
     $('.preloader').delay(1500).fadeOut('slow');
 
-    dynamicImage();
-
     $('.link').each(function () {
-        
+
         // Get the width of the parent a link for each SVG element
         var parentWidth = $(this).parent().width();
 
@@ -30,13 +72,4 @@ $(document).ready(function () {
 });
 
 $(window).on('resize', dynamicImage);
-
-// Creates a dynamic height for the paralax background image.
-
-function dynamicImage(vw) {
-
-    $('.paralax').css({
-        height: paralaxImg.innerHeight() + 100
-    });
-
-}
+$(window).on("scroll", uxFade);
